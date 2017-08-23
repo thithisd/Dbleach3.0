c8815d8f3cc119079c523358bebfee92834f8d89
thithisd <thithisd@hotmail.com>
2017-08-23 12:39:05 -0300

    4.0

--- otclientrc.lua
+++ otclientrc.lua
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 -- this file is loaded after all modules are loaded and initialized
 -- you can place any custom user code here
 
 print 'Startup done :]'
 
-=======
--- this file is loaded after all modules are loaded and initialized
--- you can place any custom user code here
-
-print 'Startup done :]'
-
->>>>>>> origin/master
